ab8bfe3086b735d3b095e44f7a5d1333987d3812
vynfred <hellovynfred@gmail.com>
2025-04-08 19:06:02 -0400

    Fix sidebar user profile navigation, date/data toggles visibility, and sidebar toggle button shape

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import InvestorDashboardWithModules from './components/Dashboard/InvestorDashboa
 import LeadsDashboard from './components/Dashboard/LeadsDashboard';
 import MarketingDashboard from './components/Dashboard/MarketingDashboard';
 import PartnerDashboard from './components/Dashboard/PartnerDashboard';
+import UserProfile from './components/dashboard/UserProfile';
 import ComponentsExamples from './components/examples/ComponentsExamples';
 import CssExamples from './components/examples/CssExamples';
 import FormExamples from './components/examples/FormExamples';
